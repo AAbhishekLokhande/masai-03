eb32288377cca7db3a01873c4c4d2ada191b5b02
Your Name <abhisheklokhande02022gmail.com>
2022-10-19 13:35:33 +0530

    login credential

---
+++ login.js
@@ -0,0 +1,11 @@
+reg user= abhi@gmail.com;
+reg pass= abc123;
+//login
+input user = abhi@gmail.com;
+input pass = abc123;
+if (reg user==input user && reg pass==input pass){
+console.log("login successfully ");
+}
+else{
+console.log("wrong crendential");
+}
